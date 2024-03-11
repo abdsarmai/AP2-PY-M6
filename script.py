c16c2b7a3f46c36cadbc608d7e5fa0ebbcd72fdd
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-03-11 20:50:25 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,34 @@
-#Avoir installé pip install ldap3
-from ldap3 import Server, Connection, SIMPLE, SYNC, SUBTREE
+#!/usr/bin/python3
+from ldap3 import Server, Connection, ALL_ATTRIBUTES
 
-def authenticate_user(username, password):
-    ldap_server = Server('adresse_du_serveur_LDAP', port=389)
+def get_ldap_users(server_address, username, password):
+    try:
+        # Configuration de l'adresse et du port du serveur LDAP
+        ldap_server = Server(server_address, port=389)
 
-    ldap_connection = Connection(ldap_server, user='DOMAIN\\' + username, password=password, authentication=SIMPLE, auto_bind=True)
+        # Création de la connexion avec le serveur LDAP
+        ldap_connection = Connection(ldap_server, user=username, password=password, auto_bind=True)
 
-    if ldap_connection.bind():
-        print("Authentification réussie")
-        return True
-    else:
-        print("Échec de l'authentification")
-        return False
+        ldap_connection.search(search_base='dc=example,dc=com',
+                               search_filter='(objectClass=user)',
+                               search_scope='SUBTREE',
+                               attributes=ALL_ATTRIBUTES)
 
-username = input("Entrez votre identifiant : ")
-password = input("Entrez votre mot de passe : ")
+        # Affichage des utilisateurs
+        for entry in ldap_connection.response:
+            if 'dn' in entry:
+                print("Utilisateur trouvé:", entry['dn'])
 
-authenticate_user(username, password)
+        # Fermeture de la connexion
+        ldap_connection.unbind()
+
+    except Exception as e:
+        print(f"Une erreur s'est produite : {e}")
+
+# Informations de connexion au serveur LDAP
+server_address = 'ldap://cleanergyo'
+username = 'cn=admin,dc=example,dc=com'
+password = 'password'
+
+# Appel de la fonction pour récupérer les utilisateurs LDAP
+get_ldap_users(server_address, username, password)
