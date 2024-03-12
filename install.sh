8272b0aef640ef2382eb971ef8987b214eb82310
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-03-12 06:07:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 apt-get update && apt-get upgrade
 apt install packagekit samba-common-bin sssd-tools sssd libnss-sss libpam-sss policykit-1 sssd ntpdate ntp realmd mariadb-server ufw openssh-server openssh-client
 apt-get update && apt-get upgrade
