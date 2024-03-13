45c5e058c67d93c0fb96239549a02941eabca4fd
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-03-13 10:36:04 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,34 +1,18 @@
-#!/usr/bin/python3
-from ldap3 import Server, Connection, ALL_ATTRIBUTES
+from ldap3 import Server, Connection, SIMPLE, SYNC, ALL
 
-def get_ldap_users(server_address, username, password):
+def authenticate_user(username, password):
+    ldap_server = Server('192.168.15.100')  # Remplacez 'ldap_server_address' par l'adresse de votre serveur LDAP
     try:
-        # Configuration de l'adresse et du port du serveur LDAP
-        ldap_server = Server(server_address, port=389)
-
-        # Création de la connexion avec le serveur LDAP
-        ldap_connection = Connection(ldap_server, user=username, password=password, auto_bind=True)
-
-        ldap_connection.search(search_base='dc=example,dc=com',
-                               search_filter='(objectClass=user)',
-                               search_scope='SUBTREE',
-                               attributes=ALL_ATTRIBUTES)
-
-        # Affichage des utilisateurs
-        for entry in ldap_connection.response:
-            if 'dn' in entry:
-                print("Utilisateur trouvé:", entry['dn'])
-
-        # Fermeture de la connexion
+        # Création d'une connexion LDAP
+        ldap_connection = Connection(ldap_server, user='cleanergyo.local\\' + username, password=password, authentication=SIMPLE, auto_bind=True)
+        print("Authentification réussie")
         ldap_connection.unbind()
-
+        return True
     except Exception as e:
-        print(f"Une erreur s'est produite : {e}")
+        print(f"Échec de l'authentification : {e}")
+        return False
 
-# Informations de connexion au serveur LDAP
-server_address = 'ldap://cleanergyo'
-username = 'cn=admin,dc=example,dc=com'
-password = 'password'
-
-# Appel de la fonction pour récupérer les utilisateurs LDAP
-get_ldap_users(server_address, username, password)
+# Utilisation de la fonction d'authentification
+username = 'your_username'
+password = 'your_password'
+authenticate_user(username, password)
