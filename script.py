c936410e2c520f3e6482d7ba0ac23cbd380a9931
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-03-18 16:37:53 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python3
-from ldap3 import Server, Connection, SIMPLE, SYNC, ALL
-
-def authenticate_user(username, password):
-    ldap_server_address = 'ldap://your_ldap_server_address:389'  # Remplacez par l'adresse de votre serveur LDAP
-    try:
-        ldap_server = Server(ldap_server_address, get_info=ALL)
-        ldap_connection = Connection(ldap_server, user=f"cn={username},ou=users,dc=example,dc=com", password=password, authentication=SIMPLE, auto_bind=True)
-        print("Authentification réussie")
-        ldap_connection.unbind()
-        return True
-    except Exception as e:
-        print(f"Échec de l'authentification : {e}")
-        return False
-
-# Utilisation de la fonction d'authentification
-username = 'adminsys'
-password = 'BtsSio15'
-authenticate_user(username, password)
+from pyad import *
+pyad.set_defaults(ldap_server="cleanergyo.local", username="adminsys", password="BtsSio15")
+user = pyad.aduser.ADUser.from_cn("myuser")
