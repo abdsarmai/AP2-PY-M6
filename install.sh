f61c761cb9c6b53471a750d0f131c13affd23dd6
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-03-20 17:40:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 apt-get update && apt-get upgrade
-apt install packagekit samba-common-bin sssd-tools sssd libnss-sss libpam-sss policykit-1 sssd ntpdate ntp realmd mariadb-server ufw openssh-server openssh-client
-apt-get update && apt-get upgrade
+sudo sed -i -e 's/#GRUB_TERMINAL/GRUB_TERMINAL/g' /etc/default/grub
